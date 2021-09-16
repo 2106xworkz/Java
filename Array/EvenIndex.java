ecb792ad1b9a829ed318c5b0c0aca076852d5d76
2106xworkz <89856489+2106xworkz@users.noreply.github.com>
2021-09-16 16:07:22 +0530

    Add files via upload

--- Array/EvenIndex.java
+++ Array/EvenIndex.java
@@ -1,10 +1,11 @@
- class Sum{
+ class EvenIndex{
 		public static void main (String[] args){
 			int A[]={12,13,14,15,16};
 			int s=0;
 			 for(int i=0;i<A.length;i++){
-			  s=s+A[i];
-			  System.out.println(s);
+			  if(A[i]%2==0)
+			 
+			  System.out.println(A[i]);
 			 }
 		}
 }
---
+++ Array/NumDiv3.java
@@ -0,0 +1,13 @@
+class NumDiv3{
+	public static void main(String[]args){
+		int A[]={12,20,33,45,50,60};
+		 for(int i=0;i<A.length;i++){ 
+		      if(A[i]%3==0) 
+		 
+		System.out.println(A[i]);
+         }	 
+	 }
+	 
+
+
+}
