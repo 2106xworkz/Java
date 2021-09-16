09a2391baf03c7f23f3385c9c4663cf3325f2eb5
2106xworkz <89856489+2106xworkz@users.noreply.github.com>
2021-09-16 16:13:48 +0530

    Add files via upload

---
+++ Array/ArraySum.java
@@ -0,0 +1,11 @@
+ class ArraySum{
+		public static void main (String[] args){
+			int A[]={12,13,14,15,16};
+			int s=0;
+			 for(int i=0;i<A.length;i++){
+			  s=s+A[i];
+			 }
+			  System.out.println(s);
+			 
+		}
+}
