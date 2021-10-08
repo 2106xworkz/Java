6cb9728ccff1f1f37ddd9fe6d6322e518e371f3c
2106xworkz <89856489+2106xworkz@users.noreply.github.com>
2021-10-08 14:42:26 +0530

    Update Email.java

--- String/Email.java
+++ String/Email.java
@@ -1,1 +1,14 @@
-
+public class Email {
+	
+ public static void main(String[]args) {
+	String S = "abc@gmail.com";
+	
+	if(S.endsWith(".com")&& S.contains("@"))
+	{
+     System.out.println("A Valid email");
+	  }else
+	  {
+		  System.out.println("Invalid email");
+	  }	
+}
+}
